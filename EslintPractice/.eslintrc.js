351ae9f04c147bac6e413d6535f9ba4a7a9d7464
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2023-06-15 14:20:48 +0530

    eslint

---
+++ EslintPractice/.eslintrc.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//   env: {
+//     browser: true,
+//     commonjs: true,
+//     es2021: true,
+//   },
+//   extends: "airbnb-base",
+//   parserOptions: {
+//     ecmaVersion: "latest",
+//   },
+//   rules: {
+//     quotes: ["error", "double"],
+//     "no-console": [0],
+//     semi: ["error", "always"],
+//     "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+
+//   },
+// };
---
+++ EslintPractice/index.js
@@ -0,0 +1,9 @@
+const showMessage = () => {
+  const a = 10;
+  const message = "I love ESLint";
+  for (let i = 0; i < 5; i++) {
+    console.log(message);
+    console.log(a);
+  }
+};
+showMessage();
